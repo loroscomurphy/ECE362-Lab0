ffd4f669b49c1b641c9579d4789d1155deb0c958
Orosco-Murphy, Logan Wyatt <lorosco@iu.edu>
2024-08-22 00:02:13 -0400

    Second commit

---
+++ lab0/src/main.c
@@ -0,0 +1,17 @@
+#include "stm32f0xx.h"
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+
+    for(;;)
+    {
+        if(x == 10)
+        {
+            y++;
+            x = 0;
+        }
+    }
+    return 0;
+}
